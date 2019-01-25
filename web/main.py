bd68a4eedf580d9c9f3517726ec7a7cf99589f48
Florencia B <fbuschiazzo@loschilis.cl>
2019-01-25 17:41:41 -0300

    used websockets currently trying to make a graph with them

--- web/main.py
+++ web/main.py
@@ -1,11 +1,23 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
+import random
+import time
+import threading
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'frc6955!'
 socketio = SocketIO(app)
 
+threadsim = None
 num_clients = 0
+
+def simular_evento():
+    with app.app_context():
+        while True:
+            numero = random.randint(0, 500)
+            print(numero)
+            emit("receive_data", {"data": numero})
+            time.sleep(0.020)
 
 @app.route('/')
 def index():
@@ -13,7 +25,10 @@ def index():
     
 @socketio.on('connect_event', namespace='/webui')
 def connect_event(payload):
-    global num_clients
+    global num_clients, threadsim
+    if threadsim is None:
+        threadsim = threading.Thread(target=simular_evento)
+        threadsim.start()
     num_clients += 1
     print("Conexiones hasta el momento:", num_clients)
     emit('client_connected', {
