b7c00f73b9c2c3a9f286e2fd4c2968f47a7588f6
Lucas Alvarez <llalvarez@uc.cl>
2019-01-29 02:16:48 -0300

    Refactored and commented JS for WebUI Remove inline JS from index.html and placed in separate JS file Added MQTT library in Gradle file (repo and file) Added basic send capabilities from Robot to MQTT. Pending testing.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,15 +1,29 @@
 package frc.robot;
 
+import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj.TimedRobot;
+
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.eclipse.paho.client.mqttv3.MqttPersistenceException;
+
 import edu.wpi.first.cameraserver.CameraServer;
 
 public class Robot extends TimedRobot {
-  
-  
 
+  MqttClient client;
+  
   @Override
   public void robotInit() {
     CameraServer.getInstance().startAutomaticCapture();
+    try {
+      client = new MqttClient("tcp://chilivision.local:1883", MqttClient.generateClientId());
+      client.connect();
+      // client.subscribe("webui/battery");
+    } catch (MqttException e) {
+      e.printStackTrace();
+    }
   }
 
   @Override
@@ -28,6 +42,20 @@ public class Robot extends TimedRobot {
 
   @Override
   public void teleopPeriodic() {
+    double voltage = RobotController.getBatteryVoltage();
+    System.out.println(voltage);
+    MqttMessage message = new MqttMessage();
+    String svoltage = Double.toString(voltage);
+    message.setPayload(svoltage.getBytes());
+    try {
+      client.publish("webui/battery/voltage", message);
+    } catch (MqttPersistenceException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    } catch (MqttException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+	  }
 
   }
 
---
+++ web/static/scripts/chiliwebui.js
@@ -0,0 +1,55 @@
+$(document).ready(function() {
+    /**
+     * Codigo de inicializacion. Aqui se prepara WebSockets, Plotly.JS y los Canvas
+     */
+    // Obtain canvas object for battery voltage display
+    var canvasBattery = document.getElementById("battery-animation");
+    var canvas2DBattery = canvasBattery.getContext("2d");
+    // Connect to websockets server with namespace /webui
+    namespace = '/webui';
+    var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port + namespace);
+    // Create a Plotly.JS graph with 30 null samples
+    var arrayLength = 30
+    var batteryVoltageTrace = new Array(arrayLength).fill(0);
+    Plotly.plot('graph', [{
+        y: batteryVoltageTrace,
+        mode: 'lines',
+        line: {color: '#80CAF6'}
+    }]);
+
+    /**
+     * Codigo de listeners. Aqui se declaran los listeners de eventos de WebSockets. Los listeners disponibles son:
+     *  - 'connect': Este evento se activa cuando un cliente logra una conexion exitosa por WS al servidor
+     *  - 'connection_confirmed': Evento generado por el servidor luego de reconocer una conexion exitosa
+     *  - 'receive_data': Evento generador por servidor para enviar informacion. Debiese ser separado en un evento
+     *                    rapido y uno lento (2-4 Hz vs 20Hz de refresco)
+     */
+    // On connect listener: Triggers upon a client succesfully connecting to the server
+    socket.on('connect', function() {
+        //socket.emit('connect_event', {});
+    })
+    // connection_confirmed listener: Emitted by the serving for acknowledging a succesful connection
+    socket.on('connection_confirmed', function(msg) {
+        $('#log').append('<p>Num conexion: ' + msg['client_id'] + '</p>');
+    });
+    // receive_data listener: Emitted async by the server to transmit information from simulations or roboRIO
+    socket.on("receive_data", function (payload){
+        // The msg variable is the dictionary payload and contains the keys specified by the server
+        var number = payload["data"];
+        // Battery canvas animation: Clear canvas, draw battery shape, draw 'charge' of battery proportionally
+        canvas2DBattery.clearRect(0, 0, 280, 150);
+        canvas2DBattery.fillStyle = "#000000";
+        canvas2DBattery.fillRect(0, 0, 280, 150);
+        canvas2DBattery.fillRect(280, 45, 20, 60)
+        canvas2DBattery.fillStyle = "#40FF00";
+        canvas2DBattery.fillRect(10, 10, (260 / 13) * number, 130);
+        // To plot updated battery voltage trace, add new data point
+        batteryVoltageTrace = batteryVoltageTrace.concat(number);
+        // The remove first data point to "shift" forward
+        batteryVoltageTrace.splice(0, 1);
+        // Update Plotly plot on 'graph' ID with shifted array
+        Plotly.update('graph', {
+            y: [batteryVoltageTrace]
+        });
+    });
+});
