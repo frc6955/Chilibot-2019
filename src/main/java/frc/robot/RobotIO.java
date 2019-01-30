a0f6502c7ce2188921bb8340544dfbe0c3fe070d
Picken <fpickenpack@ing.puc.cl>
2019-01-29 21:58:15 -0300

    reminder agregado

--- src/main/java/frc/robot/RobotIO.java
+++ src/main/java/frc/robot/RobotIO.java
@@ -3,12 +3,13 @@ package frc.robot;
 import edu.wpi.first.wpilibj.Joystick;
 //import edu.wpi.first.wpilibj.Encoder;
 //import edu.wpi.first.wpilibj.AnalogGyro;
-
+//import edu.wpi.first.wpilibj.PowerDistributionPanel;
 
  
 public class RobotIO {
  
 Joystick Joy;
+
 
 public RobotIO (int puertojoystick) {
 Joy = new Joystick(puertojoystick);
