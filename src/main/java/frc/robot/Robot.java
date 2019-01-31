f4b7c1324817d0cbb719cbac708c21967e3dd8a2
Lucas Alvarez <llalvarez@uc.cl>
2019-01-31 11:33:27 -0300

    Implemented MQTT reporting on robot

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -3,6 +3,9 @@ package frc.robot;
 import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj.TimedRobot;
 
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+
 import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
@@ -12,14 +15,17 @@ import edu.wpi.first.cameraserver.CameraServer;
 public class Robot extends TimedRobot {
 
   MqttClient client;
+  DecimalFormat d3f;
   
   @Override
   public void robotInit() {
     CameraServer.getInstance().startAutomaticCapture();
+    d3f = new DecimalFormat("#.###");
+    d3f.setRoundingMode(RoundingMode.HALF_UP);
     try {
-      client = new MqttClient("tcp://10.69.55.20:1883", MqttClient.generateClientId());
+      client = new MqttClient("tcp://10.69.55.20:1883", "fcef399c421740af845b8153e476533a", null);
       client.connect();
-      // client.subscribe("battery");
+      client.subscribe("battery");
     } catch (MqttException e) {
       e.printStackTrace();
     }
@@ -42,9 +48,8 @@ public class Robot extends TimedRobot {
   @Override
   public void teleopPeriodic() {
     double voltage = RobotController.getBatteryVoltage();
-    System.out.println(voltage);
     MqttMessage message = new MqttMessage();
-    String svoltage = Double.toString(voltage);
+    String svoltage = d3f.format(voltage);
     message.setPayload(svoltage.getBytes());
     try {
       client.publish("battery", message);
