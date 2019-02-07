1540683c404d8790e4237b82804305bc84ee0739
Lucas Alvarez <llalvarez@uc.cl>
2019-02-07 02:16:47 -0300

    Added tester site on /test for testing widgets on dash without robot Modified python to allow for tester

--- web/main.py
+++ web/main.py
@@ -1,8 +1,6 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
 from flask_mqtt import Mqtt 
-import random
-import threading
 
 
 app = Flask(__name__)
@@ -17,36 +15,32 @@ app.config['MQTT_TS_ENABLE'] = False
 mqtt = Mqtt(app)
 socketio = SocketIO(app, async_mode=None)
 
-threadsim = None
-thread_lock = threading.Lock()
 num_clients = 0
-refresh_period = 0.1
-
-def simular_evento():
-    while True:
-        numero = random.randint(0, 12)
-        # Normal blocking functions can't be used in an async SocketIO app
-        # For future reference, view the Flask-SocketIO example application:
-        # https://github.com/miguelgrinberg/Flask-SocketIO/blob/master/example/app.py
-        # socketio.emit("receive_data", {"data": numero}, namespace='/webui')
-        socketio.sleep(refresh_period)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/test')
+def tester():
+    return render_template('tester.html')
     
 @socketio.on('connect', namespace='/webui')
 def connect_event():
-    global num_clients, threadsim
-    with thread_lock:
-        if threadsim is None:
-            threadsim = socketio.start_background_task(simular_evento)
+    global num_clients
     mqtt.subscribe('webui/#', qos=0)
     num_clients += 1
     print("Conexiones hasta el momento:", num_clients)
     emit('connection_confirmed', {
         'client_id': num_clients
     }, namespace='/webui')
+
+@socketio.on('broadcast', namespace='/webui')
+def broadcast_echo(payload):
+    print("Received broadcast request!\n{}".format(payload))
+    emit(payload['event'], {
+        'data': payload['data']
+    }, broadcast=True, namespace='/webui')
 
 @mqtt.on_message()
 def handle_mqtt_messages(client, userdata, message):
